4257a30813024f1734da63af2779cb59f5046e74
zainulihsan58-hue <zainulihsan58@gmail.com>
2026-05-18 00:32:00 +0700

    Create layout.jsx

---
+++ app/layout.jsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'Modul Ajar Generator AI',
+  description: 'Generator RPM dan Modul Ajar'
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="id">
+      <body>{children}</body>
+    </html>
+  )
+}
